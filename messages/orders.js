cb9caabd7df534aed425b07879aa022d26bbb9eb
Dani025a <dani025a@edu.easj.dk>
2024-05-03 14:25:54 +0200

    small changes

--- messages/orders.js
+++ messages/orders.js
@@ -4,15 +4,15 @@ let orderExchange;
 let channel;
 
 async function connectToOrderExchange() {
-    const exchangeName = 'order';
+    const exchangeName = 'order_direct';
 
     if (!orderExchange || !channel) {
         try {
             channel = await connectToRabbitMQ();
             console.log(`Conneting to RabbitMQ exchange: ${exchangeName}...`)
-            orderExchange = await channel.assertExchange(exchangeName, 'fanout', {
-            durable: true
-        });
+            orderExchange = await channel.assertExchange(exchangeName, 'direct', {
+                durable: true
+            });
             console.log(`Established connection to RabbitMQ exchange: ${exchangeName}`)
         }
         catch (error) {
@@ -28,7 +28,7 @@ async function connectToOrderExchange() {
 async function publishOrderStartedEvent(orderInformation) {
     console.log(orderInformation);
     const { exchangeName, channel} = await connectToOrderExchange();
-    channel.publish(exchangeName, '', Buffer.from(JSON.stringify(orderInformation)));
+    channel.publish(exchangeName, 'order started', Buffer.from(JSON.stringify(orderInformation)));
 }
 
 export {
