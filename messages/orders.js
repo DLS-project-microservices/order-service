8f25f6e1c659c4f1a5181098b00dbe6cb77b16ff
masp1234 <mart2c04@stud.kea.dk>
2024-05-03 17:47:04 +0200

    consume payment_captured and publish order_complete added

--- messages/orders.js
+++ messages/orders.js
@@ -1,16 +1,16 @@
 import connectToRabbitMQ from "./connection.js";
 
-let orderExchange;
+let orderDirectExchange;
 let channel;
 
-async function connectToOrderExchange() {
+async function connectToOrderDirectExchange() {
     const exchangeName = 'order_direct';
 
-    if (!orderExchange || !channel) {
+    if (!orderDirectExchange || !channel) {
         try {
             channel = await connectToRabbitMQ();
             console.log(`Conneting to RabbitMQ exchange: ${exchangeName}...`)
-            orderExchange = await channel.assertExchange(exchangeName, 'direct', {
+            orderDirectExchange = await channel.assertExchange(exchangeName, 'direct', {
                 durable: true
             });
             console.log(`Established connection to RabbitMQ exchange: ${exchangeName}`)
@@ -26,11 +26,56 @@ async function connectToOrderExchange() {
 }
 
 async function publishOrderStartedEvent(orderInformation) {
-    console.log(orderInformation);
-    const { exchangeName, channel} = await connectToOrderExchange();
-    channel.publish(exchangeName, 'order started', Buffer.from(JSON.stringify(orderInformation)));
+    const { exchangeName, channel} = await connectToOrderDirectExchange();
+    try {
+        channel.publish(exchangeName, 'order started', Buffer.from(JSON.stringify(orderInformation)));
+        console.log('order_started event published successfully')
+    }
+    catch(error) {
+        console.error('Error publishing order_started message', error);
+    }
+}
+
+async function consumePaymentCaptured() {
+    const queueName = "order_service_consume_payment_captured";
+    const { exchangeName, channel } = await connectToOrderDirectExchange();
+
+    await channel.assertQueue(queueName, {
+        durable: true
+    });
+    channel.bindQueue(queueName, exchangeName, 'payment captured');
+
+    console.log('Waiting for payment_captured events...');
+
+    channel.consume(queueName, async (msg) => {
+        try {
+            if (msg?.content) {
+                const messageContent = JSON.parse(msg.content.toString());
+                    console.log(messageContent);
+                    await publishOrderCompleted(messageContent);
+                    console.log('payment_captured event processed successfully');
+                    
+                    channel.ack(msg);
+            }
+        }
+        catch(error) {
+            console.error('Error consuming payment_captured event:', error);
+        }
+    })
+}
+
+async function publishOrderCompleted(message) {
+    const { exchangeName, channel} = await connectToOrderDirectExchange();
+    try {
+        channel.publish(exchangeName, 'order completed', Buffer.from(JSON.stringify(message)));
+        console.log('order_completed event published successfully');
+    }
+    catch(error) {
+        console.error('Error publishing order_completed message', error);
+    }
 }
 
 export {
-    publishOrderStartedEvent
+    consumePaymentCaptured,
+    publishOrderStartedEvent,
 }
